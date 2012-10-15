6eb8f21487aa3d72f3c3fbe30e25b4dd37c52450
Ilya Margolin <ilya@jimdo.com>
2012-10-15 21:54:34 +0200

    The packaging challenge starts... NOW!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='python-cfn-templates',
+      version='0.1.0',
+      url='https://github.com/fungusakafungus/python-cfn-templates',
+      author='Ilya Margolin',
+      author_email='ilya@jimdo.com',
+      py_modules=['cfn'],
+      packages=['AWS'],
+      )
+
