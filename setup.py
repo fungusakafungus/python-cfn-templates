e66aaf8b349b7a9eebb0f5cdd9841cd69272ba24
Ilya Margolin <ilya@jimdo.com>
2012-10-31 21:37:18 +0100

    prepare for tag v0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='python-cfn-templates',
-      version='0.1.0',
+        version='0.2.0',
       url='https://github.com/fungusakafungus/python-cfn-templates',
       author='Ilya Margolin',
       author_email='ilya@jimdo.com',
